e45dbbd68ea73bf9db412d21fa494cb0c96a9340
Anudeep Chandra Paul <achandrapaul@manh.com>
2020-06-06 15:51:41 +0530

    Adding google verification tag

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -11,12 +11,16 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-        {/* <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;400;500;700;800&family=Roboto+Mono:wght@100;300;400;500;700&display=swap" rel="stylesheet"/> */}
+          {/* <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;400;500;700;800&family=Roboto+Mono:wght@100;300;400;500;700&display=swap" rel="stylesheet"/> */}
           <style>{`
                 body{
                     margin: 0px;
                 }
             `}</style>
+          <meta
+            name="google-site-verification"
+            content="6Sel5PCo0RnZeZQzzoRUOiQIjWreM5FXUSN-o-RWyao"
+          />
         </Head>
         <body>
           <Main />
