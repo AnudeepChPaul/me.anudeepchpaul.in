10c4268eb0732b9e0d8d52be2d04ddb1096219d5
Anudeep Chandra Paul <achandrapaul@manh.com>
2020-06-07 05:33:27 +0530

    Adding Meta tags

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,22 +2,24 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 import Link from "next/link";
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
-  }
-
   render() {
     return (
       <Html>
         <Head>
-          {/* <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;400;500;700;800&family=Roboto+Mono:wght@100;300;400;500;700&display=swap" rel="stylesheet"/> */}
           <style>{`
                 body{
                     margin: 0px;
+                    width: fit-content;
                 }
             `}</style>
-          <meta name="description" content="A portal to check on Anudeep Chandra Paul's resume and skills."/>
+          <meta
+            name="description"
+            content="A portal to check on Anudeep Chandra Paul's resume and skills."
+          />
+          <meta
+            property="og:description"
+            content="A portal to check on Anudeep Chandra Paul's resume and skills."
+          />
           <meta
             name="google-site-verification"
             content="6Sel5PCo0RnZeZQzzoRUOiQIjWreM5FXUSN-o-RWyao"
