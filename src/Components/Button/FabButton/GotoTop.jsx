7a4a88f81680ffd8a8c3634788a0a879121bd154
Anudeep Chandra Paul <achandrapaul@manh.com>
2020-06-26 13:02:02 +0530

    Added font-awesome and adopted dynamic view width

--- src/Components/Button/FabButton/GotoTop.jsx
+++ src/Components/Button/FabButton/GotoTop.jsx
@@ -1,5 +1,7 @@
 import classes from "./GotoTop.module.scss";
 import React from "react";
+import { faArrowCircleUp } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const FabButton = function (props) {
   const classNames = [
@@ -12,7 +14,7 @@ const FabButton = function (props) {
   return (
     <div className={classNames.join(" ")}>
       <button className={classes["fab-button-body"]} onClick={props.onClick}>
-        {props.text}
+        <FontAwesomeIcon icon={faArrowCircleUp} />
       </button>
     </div>
   );
