4d1c0445969d514d08d219fa35087ccc13c38fda
Anudeep Chandra Paul <achandrapaul@manh.com>
2020-07-03 17:11:58 +0530

    Initialized service workers

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -14,6 +14,7 @@ import Experiences from "@/Components/Experiences/Experiences";
 import Header from "@/Components/Header/Header";
 import { Grid, withStyles } from "@material-ui/core";
 import ProfessionalProjects from "@/Components/Projects/ProfessionalProjects";
+import Helper from "@/Helpers/Helper";
 
 const styles = () => ({
   quickLinks: {
