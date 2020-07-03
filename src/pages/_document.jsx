2f40db66eea28d15f78a08ed13534d5b028dc679
Anudeep Chandra Paul <achandrapaul@manh.com>
2020-07-03 17:38:16 +0530

    Meta theme color change

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -19,7 +19,7 @@ class MyDocument extends Document {
             name="google-site-verification"
             content="6Sel5PCo0RnZeZQzzoRUOiQIjWreM5FXUSN-o-RWyao"
           />
-          <meta name="theme-color" color="#1c2231" />
+          <meta name="theme-color" color="#000" />
           <meta name="robots" content="all" />
           <meta
             name="viewport"
